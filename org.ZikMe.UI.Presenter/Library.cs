18a23f61adbf0a2857013892ef8df6feb7dc79af
gfauredumont <faurdumont@gmail.com>
2012-02-26 18:21:55 +0100

    Looks better with SmartGit...

--- org.ZikMe.UI.Presenter/Library.cs
+++ org.ZikMe.UI.Presenter/Library.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CedricMao.Collections;
+using org.ZikMe.BusinessEntities;
 using org.ZikMe.BusinessLayer;
 
 namespace org.ZikMe.UI.Presenter
@@ -14,5 +16,22 @@ namespace org.ZikMe.UI.Presenter
         {
             _br = BusinessReferential.Instance;
         }
+
+
+        #region Properties
+
+        public ViList<IArtist> Artists
+        {
+            get
+            {
+                if (_br != null)
+                    return _br.Artists;
+
+                return null;
+            }
+        }
+
+        #endregion Properties
+
     }
 }
