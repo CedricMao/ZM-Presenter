7727185127bfad7977436e789b82b7dd20f971e9
Guillaume FAURE-DUMONT <GFD@.(none)>
2012-02-26 13:11:38 +0100

    Second Commit

---
+++ test.cs
@@ -0,0 +1,21 @@
+onfig --global user.name "FAURE-DUMONT"
+  git config --global user.email fauredumont@gmail.com
+      
+Next steps:
+  mkdir ZM-Presenter
+  cd ZM-Presenter
+  git init
+  touch README
+  git add README
+  git commit -m 'first commit'
+  git remote add origin git@github.com:gfauredumont/ZM-Presenter.git
+  git push -u origin master
+      
+Existing Git Repo?
+  cd existing_git_repo
+  git remote add origin git@github.com:gfauredumont/ZM-Presenter.git
+  git push -u origin master
+      
+Importing a Subversion Repo?
+  Click here
+      
