fc2f9c57e16b9abcc0d1ab762c47c218235d092a
gfauredumont <faurdumont@gmail.com>
2012-02-26 18:07:48 +0100

    Adding lots of files, trying a USABLE client (SmartGit) !

---
+++ org.ZikMe.UI.Presenter/Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.ZikMe.BusinessLayer;
+
+namespace org.ZikMe.UI.Presenter
+{
+    public class Library
+    {
+        private BusinessLayer.BusinessReferential _br = null;
+
+        public Library()
+        {
+            _br = BusinessReferential.Instance;
+            
+        }
+    }
+}
